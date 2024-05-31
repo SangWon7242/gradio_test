ecaaa6299fa2542a75a799e6665f670c83bd3001
SangWon7242 <gotkdqja@gmail.com>
2024-05-31 18:07:42 +0900

    ChatInterface 데모 코드 구현

--- readme.md
+++ readme.md
@@ -45,3 +45,35 @@ demo.launch(share=True) # `share=True` 데모용 URL 생성
 
 # 사용시 데모 URL이 생성되어 다른 사람과 공유가 가능!
 ```
+
+# 빠르게 개발하는 모드로 전환
+
+- [참고자료](https://www.gradio.app/guides/developing-faster-with-reload-mode)
+
+```
+# 명령어
+gradio run.py
+```
+
+# 개발 모드로 전환하기 위한 코드 작성
+
+## ChatInterface 데모 코드 구현
+
+```py
+demo = gr.ChatInterface(
+    yes_man,
+    chatbot=gr.Chatbot(height=300),
+    textbox=gr.Textbox(placeholder="Ask me a yes or no question", container=False, scale=7),
+    title="Yes Man",
+    description="Ask Yes Man any question",
+    theme="soft",
+    examples=["파이썬이란?", "객체지향 프로그래밍이란?", "프로그래밍 공부는 어떻게 해야하는가?"],
+    cache_examples=True,
+    retry_btn=None,
+    undo_btn="Delete Previous",
+    clear_btn="Clear",
+)
+
+if __name__ == "__main__":
+    demo.launch()
+```
--- src/gr_test.py
+++ src/gr_test.py
@@ -1,12 +1,24 @@
 import gradio as gr
 
-def greet(name, intensity):
-    return "Hello, " + name + "!" * int(intensity)
+def yes_man(message, history):
+    if message.endswith("?"):
+        return "Yes"
+    else:
+        return "Ask me anything!"
 
-demo = gr.Interface(
-  fn=greet,
-  inputs="textbox",
-  outputs="textbox"
+demo = gr.ChatInterface(
+    yes_man,
+    chatbot=gr.Chatbot(height=300),
+    textbox=gr.Textbox(placeholder="Ask me a yes or no question", container=False, scale=7),
+    title="Yes Man",
+    description="Ask Yes Man any question",
+    theme="soft",
+    examples=["파이썬이란?", "객체지향 프로그래밍이란?", "프로그래밍 공부는 어떻게 해야하는가?"],
+    cache_examples=True,
+    retry_btn=None,
+    undo_btn="Delete Previous",
+    clear_btn="Clear",
 )
 
-demo.launch(share=True) # 데모용 URL 생성
+if __name__ == "__main__":
+    demo.launch()
